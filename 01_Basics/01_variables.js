ede07c12c8c0c6f6fd8d73c69040aedce7f0093c
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-01-29 10:07:32 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accId = "1250"
+let emailId  = "xtz30@.com"
+var accCity = "Madras"
+let AccountState;
+
+/*
+Prefer not to use Var Because of Block Scope and Functional Scope...
+*/
+
+
+emailId = "xyz@.com"
+accCity = "ODisha"
+PassWord = "30"
+
+
+console.table([accId,emailId,accCity,PassWord,AccountState])
