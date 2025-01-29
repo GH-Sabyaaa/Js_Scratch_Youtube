392b4ee87a3871586344dc4c464005ea654d887a
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-01-29 17:29:03 +0000

    Conversion  of DataTypes and Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -29,3 +29,37 @@ let someNumber = 69
 let stringNumber = String(someNumber)
 console.log(stringNumber);  // Seems like Number but it Turns into String
 console.log(typeof stringNumber);
+
+// ******************************** Operations *******************************
+
+
+let value = 7
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Helo"
+let str2 = " SabyA"
+let str3 = str1 + str2 
+
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+
+// console.log( (3 + 4) * 5 % 3)
+
+let GameCounter = 100
+// GameCounter++;
+++GameCounter;
+// console.log(GameCounter);
+console.log(GameCounter);
