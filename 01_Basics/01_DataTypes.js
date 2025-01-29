88bd401a886168614708e4a90ab05304e5aeab4b
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-01-29 10:43:07 +0000

    datatypes

---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)  we are using node js not broswer
+
+console.log(3
+    +
+    3)  // Code Readability should be High
+
+
+console.log("OTC")
+
+let name = "SabyA"
+let age = 20
+let isloggedIn = false
+let state;
+
+
+// number => 2 to power 53
+//bigint
+// string => " "
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof age); //number
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
