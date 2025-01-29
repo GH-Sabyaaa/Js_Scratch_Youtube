327cca2cdb3d9e0c62df86ee39741aa795a142ec
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-01-29 11:04:19 +0000

    conversions of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "30xtz"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"30" => 33
+// "30xtz" => NaN
+// true => 1  ; false => 0
+
+
+
+let isLoggedIn = "kkk"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// " " => false
+// "SabyA" => true
+
+
+let someNumber = 69
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);  // Seems like Number but it Turns into String
+console.log(typeof stringNumber);
