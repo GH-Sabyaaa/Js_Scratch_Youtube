f87cd41165b0c1bf671a0f40e5eebefb448519a0
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-01-30 08:46:31 +0000

    dataType summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean , null , undefined , Symbol , BigInt
+
+ const score = 300
+ const scorevalue = 300.3
+
+ const isLogggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherid = Symbol('123' );
+ 
+ console.log(id === anotherid);
+
+
+ const bigNumber = 12542125512552555125n
+ 
+// Reference (Non  primitive)
+   
+// Array , Objects , Functions
+
+const heros = ["shaktiman", "spiderman" , "superman"]
+
+ let myObj = {      // Object
+    name: "SabyA",
+    age: 20,
+}
+
+
+ const myFunction = function(){      // Function 
+    console.log("Hello Bhai");
+    
+ }
+
+ console.log(typeof bigNumber);  // bigint
+ console.log(typeof outsideTemp);  // Type of null is Object...
+ console.log(typeof myFunction);  // function
+ console.log(typeof anotherid);  // symbol
+
+  
+
+ 
