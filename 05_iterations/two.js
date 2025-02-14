e9f6bc0eab809bfc4fba4cb11fd6a2ceb1181b1f
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-02-14 06:03:39 +0000

    While & DoWhile Loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// While Loop 
+
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index++
+    
+}
+
+let myArr = ['OTC', "Architech", "Lunatic"]
+let i = 0
+while(i< myArr.length){
+    // console.log(`i am ${myArr[i]}`);
+    i++
+    
+}
+
+
+// Do While Loop
+
+let score = 15
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10);
