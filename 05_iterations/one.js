8ce3dca052c4c070e9542eb15a0ab6319fd1b14a
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-02-14 05:55:05 +0000

    For Loop , Break and Continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,37 @@
+// for
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if(element == 7){
+        // console.log("7 is Thala for a Reason");
+        
+    }
+    // console.log(element);
+    
+    
+}
+
+
+
+let myArr = ['OTC', "Final Boss", "YEET"]
+// console.log(myArr.length);
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    // console.log(element)
+    
+}
+
+// break and Continue
+
+
+for (let index = 1; index < 25; index++) {
+    if(index == 7){
+        console.log(`Detected 7`);
+        continue
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+    
+}
