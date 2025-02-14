4a40c34603ff4ef0d699d026cdefb9585b0bd319
GH-Sabyaaa <sabyasachinayak845@gmail.com>
2025-02-14 07:51:59 +0000

    map and chaining

---
+++ 05_iterations/six.js
@@ -0,0 +1,23 @@
+// const coding = ["java", "ruby", "js", "python", "cpp"]
+
+// const values =    coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+    
+// })
+
+// console.log(values);
+
+
+const myNum = [ 1,2,3,4,5,6,7,8,,9,10]
+
+// const newNums = myNum.filter( (num) => num > 4 )
+// console.log(newNums);
+
+// const newNumm = myNum.filter((num) => {
+//     return num > 5 // if You Open aScope/ Curly Braces Then you have to use return keyWord 
+// })
+// console.log(newNumm);
+
+
+
